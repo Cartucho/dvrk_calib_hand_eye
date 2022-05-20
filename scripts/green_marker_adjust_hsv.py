0c3a4594b894532228d1b8a520a627a8ea73e870
Cartucho <joaocartuchoo@gmail.com>
2022-05-20 11:07:15 +0100

    get path of data

--- scripts/green_marker_adjust_hsv.py
+++ scripts/green_marker_adjust_hsv.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
 import sys
+import os
 
 import rospy
 import rospkg
 
 class AdjustHSV:
-  def __init__(self):
-    print("heyyy")
+  def __init__(self, path_images, path_green_marker_data):
+    print(path_images)
+    print(path_green_marker_data)
 
 
 def main(args):
   rospy.init_node('green_marker_adjust_hsv', anonymous=True)
-  a = AdjustHSV()
+  rospack = rospkg.RosPack()
+  path_package = rospack.get_path('dvrk_calib_hand_eye')
+  path_images = os.path.join(path_package, 'data', 'cam_interpolated')
+  path_green_marker_data = os.path.join(path_package, 'cylmarker', 'data')
+  a = AdjustHSV(path_images, path_green_marker_data)
   try:
     rospy.spin()
   except KeyboardInterrupt:
